5160d1bf9b502a8e11895277fe51cea90ace39d9
Cesar Guerron <guerronc@outlook.com>
2019-07-06 20:59:40 -0500

    Agregar funcionalidad con react router dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,20 @@
-import React, { Component } from "react";
-import Axios from "axios";
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Menu from "./Menu";
+import Usuarios from "./Usuarios/index";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      usuarios: []
-    };
-  }
+const Tareas = () =>(
+  <div>
+    Tareas
+  </div>
+);
 
-  async componentDidMount() {
-    const respuesta = await Axios.get(
-      "https://jsonplaceholder.typicode.com/users"
-    );
-    this.setState({
-      usuarios: respuesta.data
-    });
-  }
-
-  ponerFilas = () =>
-    this.state.usuarios.map(usuario => (
-      <tr key={usuario.id}>
-        <td>{usuario.name}</td>
-        <td>{usuario.email}</td>
-        <td>{usuario.website}</td>
-      </tr>
-    ));
-
-  render() {
-    console.log(this.state.usuarios);
-    return (
-      <div className="margen">
-        <table className="tabla">
-          <thead>
-            <tr>
-              <th>Nombre</th>
-              <th>Correo</th>
-              <th>Enlace</th>
-            </tr>
-          </thead>
-          <tbody>{this.ponerFilas()}</tbody>
-        </table>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <Menu />
+    <Route exact path="/" component={Usuarios} />
+    <Route exact path="/tareas" component={Tareas} />
+  </BrowserRouter>
+);
 
 export default App;
