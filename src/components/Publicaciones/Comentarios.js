6663e78b67985c6826de1492704c2693b03e94eb
Cesar Guerron <guerronc@outlook.com>
2019-07-14 15:17:01 -0500

    Agregar componente comentarios

---
+++ src/components/Publicaciones/Comentarios.js
@@ -0,0 +1,17 @@
+//Componente funcional o state less
+
+import React from "react";
+
+const Comentarios = props => {
+  return (
+    <ul>
+      <li>Hola</li>
+
+      <li>Hola</li>
+
+      <li>Hola</li>
+    </ul>
+  );
+};
+
+export default Comentarios;
