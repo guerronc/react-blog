3acb625d7cfcae6c84f76ba5f2b1dac9606c0a67
Cesar Guerron <guerronc@outlook.com>
2019-07-14 20:42:03 -0500

    Agregar metodos para buscar las tareas por usuario

--- src/components/Tareas/index.js
+++ src/components/Tareas/index.js
@@ -1,19 +1,59 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import * as tareasActions from '../../actions/tareasActions';
+import * as tareasActions from "../../actions/tareasActions";
+import Spinner from "../General/Spinner";
+import Fatal from "../General/Fatal";
 
 class Tareas extends Component {
-
-  componentDidMount(){
+  componentDidMount() {
     this.props.traerTodas();
   }
 
+  mostrarContenido = () => {
+    const { tareas, cargando, error } = this.props;
+
+    if (error) {
+      return <Fatal mensaje={error} />;
+    }
+
+    if (cargando) {
+      return <Spinner />;
+    }
+
+    return Object.keys(tareas).map(user_id => (
+      <div key={user_id}>
+        <h2>Usuario {user_id}</h2>
+        <div className="contenedor_tareas">{this.ponerTareas(user_id)}</div>
+      </div>
+    ));
+  };
+
+  ponerTareas = user_id => {
+    const { tareas } = this.props;
+    const por_usuario = {
+      ...tareas[user_id]
+    };
+
+    return Object.keys(por_usuario).map(tarea_id => (
+      <div key={tarea_id}>
+        <input
+          type="checkbox"
+          defaultChecked={por_usuario[tarea_id].completed}
+        />
+        {por_usuario[tarea_id].title}
+      </div>
+    ));
+  };
+
   render() {
     console.log(this.props);
-    return <div>Tareas saludar</div>;
+    return <div>{this.mostrarContenido()}</div>;
   }
 }
 
-const mapStateToProps = ({tareasReducer}) => tareasReducer; 
+const mapStateToProps = ({ tareasReducer }) => tareasReducer;
 
-export default connect(mapStateToProps, tareasActions)(Tareas);
+export default connect(
+  mapStateToProps,
+  tareasActions
+)(Tareas);
